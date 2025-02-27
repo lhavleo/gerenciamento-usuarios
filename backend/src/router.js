5b74002a3629d84a24c75b8a653580d78f05bae3
Leonardo Valin <leonardo@serrabits.com.br>
2025-02-26 21:52:53 -0300

    Iciando a criacao da API

---
+++ backend/src/router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+//router.get('/users', (req, res) => res.status(200).send('Router funcionando'));
+
+module.exports = router;
